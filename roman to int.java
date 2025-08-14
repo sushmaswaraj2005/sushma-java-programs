ee295b124a0af7aecfe536707b6117d19f49e662
sushma swaraj <velurusushma45@gmail.com>
2025-08-14 10:13:53 +0530

    Add files via upload

---
+++ roman to int.java
@@ -0,0 +1,26 @@
+import java.util.HashMap;
+import java.util.Map;
+class RomantoInt{
+    public static void main(String[]args){
+        Map<Character,Integer>map=new HashMap<>();
+        map.put('I',1);
+        map.put('V',5);
+        map.put('X',10);
+        map.put('L',50);
+        map.put('C',100);
+        map.put('D',500);
+        map.put('M',1000);
+        String s="MCMIV";
+        int result=0;
+        for(int i=0;i<s.length();i++){
+            int value=map.get(s.charAt(0));
+            if(i+1<s.length() && value<map.get(s.charAt(i+1))){
+                result-=value;
+            }
+            else{
+                result+=value;
+            }
+        }
+        System.out.println("int value:"+result);
+    }
+}
